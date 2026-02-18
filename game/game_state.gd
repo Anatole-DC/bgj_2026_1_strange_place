2eb51a458083fce46f203934cc9afaf9dc57a6aa
Anatole-DC <adechauveron@gmail.com>
2026-02-18 10:50:44 +0100

    wip(quests): new hud and quests tracking

--- game/game_state.gd
+++ game/game_state.gd
@@ -3,10 +3,49 @@ extends Node
 enum Day {FIRST, SECOND, LAST, END}
 var current_day: Day = Day.FIRST
 
+
 # First day
-var has_post_it: bool = false
+@export_group("Day 1")
+@export_subgroup("D1 Mailing")
+@export var d1_mailing: Control
+@export var d1_mailing_task: CheckBox
+var d1_completed_email_tasks: bool = false:
+	set(value):
+		d1_completed_email_tasks = value
+		if (value):
+			mark_task_complete(d1_mailing_task)
+			d1_mailing.hide()
+
+
+@export var brian_post_it_task: CheckBox
+var has_post_it: bool = false:
+	set(value):
+		has_post_it = value
+		if (value): mark_task_complete(brian_post_it_task)
+
+@export_subgroup("D1 Calling")
+@export var d1_calling: Control
+@export var d1_calling_task: CheckBox
+var d1_completed_calling_tasks: bool = false:
+	set(value):
+		d1_completed_calling_tasks = value
+		if (value):
+			mark_task_complete(d1_calling_task)
+			d1_calling.hide()
+
 var called_health_office: bool = false
-var d1_completed_email_tasks: bool = false
+
+@export var checked_budget_task: CheckBox
+var checked_budget: bool = false:
+	set(value):
+		checked_budget = value
+		if (value): mark_task_complete(checked_budget_task)
+
+@export var find_file_task: CheckBox
+var find_file: bool = false:
+	set(value):
+		find_file = value
+		if (value): mark_task_complete(find_file_task)
 
 # Second day
 var called_disaster_management: bool = false
@@ -21,3 +60,7 @@ func go_next_day(previous_day: Day):
 		return
 	current_day = Day.END
 	return
+
+func mark_task_complete(checkbox: CheckBox):
+	checkbox.disabled = true
+	checkbox.button_pressed = true
