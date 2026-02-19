82a41a51b018c1600b6a442ae1b33e31e7f1cf1e
Anatole-DC <adechauveron@gmail.com>
2026-02-19 12:41:49 +0100

    feat(ux): highlight interactables

--- game/entities/furnitures/budget/budget.gd
+++ game/entities/furnitures/budget/budget.gd
@@ -2,15 +2,10 @@ extends Area2D
 
 @export_file("*.dtl") var budget_dialogue: String
 
-func _ready():
-	if not GameState.checked_budget: return
-	queue_free()
-
 func _on_input_event(viewport, event, shape_idx):
 	if event is not InputEventMouseButton: return
 	if Dialogic.current_timeline != null: return
 	
 	if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		GameState.checked_budget = true
-		queue_free()
 		Dialogic.start(budget_dialogue)
